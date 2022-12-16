2e26c3bd95c2f3ea49d023bf5ccb3b8e41a5f6d6
tarun291 <tarunkumar222020@gmail.com>
2022-12-16 18:27:14 +0530

    Added Create ans Delete City File within repository folder

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const bodyParser=require("body-parser");
 
 const { PORT }=require('./config/serverConfing');
 
+const CityRepository=require('./repository/city-repository');
+
+
 const setupAndStartServer=async ()=>{
     // create the express object
     const app=express();
@@ -11,6 +14,9 @@ const setupAndStartServer=async ()=>{
     app.use(bodyParser.urlencoded({extended:true}));
     app.listen(PORT,()=>{
         console.log(`Server started at ${PORT}`);
+        const repo=new CityRepository();
+        // console.log(repo.createCity({name:"Agra"}));
+        repo.deleteCity({cityId:1});
     })
 }
 setupAndStartServer();
